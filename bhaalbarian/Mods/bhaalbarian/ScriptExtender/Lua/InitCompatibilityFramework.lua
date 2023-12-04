c2ef1f0cd274bdad894e8040b3c8762df014cff2
Ken Bynell <ken.bynell@gmail.com>
2023-12-04 17:40:46 -0600

    almost ready for the loca

--- bhaalbarian/Mods/bhaalbarian/ScriptExtender/Lua/InitCompatibilityFramework.lua
+++ bhaalbarian/Mods/bhaalbarian/ScriptExtender/Lua/InitCompatibilityFramework.lua
@@ -6,7 +6,7 @@ if Ext.Mod.IsModLoaded("67fbbd53-7c7d-4cfa-9409-6d737b4d92a9") then
       khbsd_Springtime = {
         modGuid = "65cb95be-5999-4165-ad50-38b97dfb8416",
         subClassGuid = "6eba49be-a5c7-431f-857d-b4576d453e56",
-        class = "monk",
+        class = "barbarian",
         subClassName = "Hearteater"
       }
     }
